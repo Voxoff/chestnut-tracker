d670939dc515ceeb16c4d59a06e727d6e551c07a
Guy Bennett-Jones <guybennettjones@hotmail.co.uk>
2020-03-07 20:38:39 +0000

    oops

--- config/environment.rb
+++ config/environment.rb
@@ -1,9 +1,10 @@
-Rails.logger = Logger.new(STDOUT)
-# Rails.logger.level = Logger::DEBUG
-Rails.logger.datetime_format = "%Y-%m-%d %H:%M:%S"
 
 # Load the Rails application.
 require_relative 'application'
+
+Rails.logger = Logger.new(STDOUT)
+# Rails.logger.level = Logger::DEBUG
+Rails.logger.datetime_format = "%Y-%m-%d %H:%M:%S"
 
 # Initialize the Rails application.
 Rails.application.initialize!
